17cd57ad1d9e1dc902052c5bd923fb471a0f0bdb
!resiu <!resiu@campus.stonybrook.edu>
2026-03-03 11:04:47 -0500

    style-grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import CardComponent from './components/Card';
 import './dist.css';
@@ -11,9 +9,16 @@ function App() {
   const cardsArray = Array.from({ length: cardCount });
 
   return (
-    <div className="grid grid-cols-[repeat(auto-fill,minmax(15rem,1fr))] gap-4 p-4">
+    <div
+      style={{
+        display: 'grid',
+        gridTemplateColumns: 'repeat(auto-fill, minmax(15rem, 1fr))',
+        gap: '0rem',
+        padding: '0rem',
+      }}
+    >
       {cardsArray.map((_, index) => (
-          <CardComponent key={index} />
+        <CardComponent key={index} />
       ))}
     </div>
   );
